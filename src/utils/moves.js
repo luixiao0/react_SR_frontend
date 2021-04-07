4480bc56e3122574f702167a0d70c2674f6cc3f9
lux <43348732+luixiao0@users.noreply.github.com>
2021-04-07 17:10:53 +0800

    Update moves.js

--- src/utils/moves.js
+++ src/utils/moves.js
@@ -19,6 +19,55 @@ token = (uname, psw)=>{
             "Content-Type":"application/x-www-form-urlencoded"},
         body:'username='+uname+'&password='+psw
     }).then(res=>res.json()).then(data=>{
-                 response = data
+                 Authorization = data
     })
 }
+
+auth_header = function(){
+    return Authorization.token_type + ' ' + Authorization.access_token
+} 
+
+my_status = () =>{
+    fetch('/me',{
+        headers:{
+            "accept": "application/json",
+            "Authorization": auth_header()
+        }}).then(res=>res.json()).then(data=>{
+            status = data
+        })
+}
+
+task_query = () =>{
+    fetch('/me/query',{
+        method:"post",
+        headers:{
+            "accept": "application/json",
+            "Authorization": auth_header()
+        },
+        }).then(res=>res.text()).then(data=>{
+            curtasks = JSON.parse(data);
+        })
+}
+
+
+
+
+new_task = (file) =>{
+    fetch('/me/newtask',{
+        headers:{
+            method:"post",
+            "accept": "application/json",
+            "Authorization": auth_header(),
+            'Content-Type': 'multipart/form-data'
+            },
+            body: file
+        }).then(
+            response => response.json() // if the response is a JSON object
+          ).then(
+            success => console.log(success) // Handle the success response object
+          ).catch(
+            error => console.log(error) // Handle the error response object
+          );
+}
+
+
