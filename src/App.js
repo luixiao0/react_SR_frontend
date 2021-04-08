0a8673ee79794a50b92f51b20f91db796f86dd97
lux <43348732+luixiao0@users.noreply.github.com>
2021-04-08 20:04:51 +0800

    clean

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Framebars from './utils/sidebar'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Firstpage from './pages/firstpage'
-import Tasks from './pages/tasks'
-import Stat from './pages/stat'
-import Luixiao from './pages/lux'
-import Login from './pages/login'
-
 
 function App() {
   return (
-    <div className='App'>
-    <Router>
-    
-      <Framebars/>
-      <Switch>
-        <Route path='/' exact component={Login}/>
-        <Route path='/tasks' component={Tasks}/>
-        <Route path='/me' component={Stat}/>
-        <Route path='/lux' component={Luixiao}/>
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
