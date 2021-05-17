0ee4edebd34d3f666e265b664b294d469e73b171
lux <43348732+luixiao0@users.noreply.github.com>
2021-05-17 08:27:28 +0800

    Update moves.js

--- src/utils/moves.js
+++ src/utils/moves.js
@@ -125,7 +125,7 @@ export default class Userstate {
             headers:{
               "accept": "application/json",
               "Authorization": this.tokenHeader(),
-              "Content-Type": "multipart/form-data"
+            //   "Content-Type": "multipart/form-data"
             },
             body:formData
             })
